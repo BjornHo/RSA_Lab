3baa145331e327028d16384242c43fc461553df5
B.Ho <bjornho@outlook.com>
2022-03-06 22:21:26 +0100

    Readable prints

--- tests/test_Hastad.py
+++ tests/test_Hastad.py
@@ -2,18 +2,17 @@
 
 
 import os,sys,inspect
-import secrets
 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 import unittest
+import secrets
 from RSA import *
 from exploits.Hastad import Hastad_BCA
 from sage.all import *
 
-import random
 
 class MyTestCase(unittest.TestCase):
 
