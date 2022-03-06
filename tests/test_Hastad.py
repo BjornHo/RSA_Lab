2054d77ec99729dafc9f3d10dcdc90c38208c9ec
B.Ho <bjornho@outlook.com>
2022-03-06 20:21:08 +0100

    Hastad BCA works

--- tests/test_Hastad.py
+++ tests/test_Hastad.py
@@ -2,13 +2,14 @@
 
 
 import os,sys,inspect
+
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 import unittest
 from RSA import *
-from exploits import Hastad
+from exploits.Hastad import Hastad_BCA
 from sage.all import *
 
 import random
@@ -16,7 +17,7 @@ import random
 class MyTestCase(unittest.TestCase):
 
     def test_Hastad_BCA(self):
-        # Linear padding function
+        # Public linear padding function
         #f(a_i,m,b_i) = a_i * m + b_i
         __tmp__=var("a_i,m_i,b_i");f = symbolic_expression(a_i * m_i + b_i).function(a_i,m_i,b_i)
 
@@ -28,9 +29,10 @@ class MyTestCase(unittest.TestCase):
 
         e = 3
         message = 1337
+        num_users = 3
 
-        # Generate 3 users
-        for _ in range(3):
+        # Generate users
+        for _ in range(num_users):
             user_i = gen_user(1024, e)
             user_list.append(user_i)
 
@@ -58,8 +60,13 @@ class MyTestCase(unittest.TestCase):
             c_list.append(c)
             i += 1
 
+        # Pass on list of a, b, ciphertexts of each user and modulus of each user
+        # And perform the Hastad Broadcast Attack
+        result = Hastad_BCA(a_list, b_list, c_list, n_list, e)
 
-
+        self.assertEqual(message, result)
+        print("Hastad Broadcast Attack Successful")
+        print("The secret message is: " + str(result))
 
 
 if __name__ == '__main__':
