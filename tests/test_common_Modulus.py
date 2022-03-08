683d1cd7a7d790fd3dcf75ae1867b24cc23b9410
B.Ho <bjornho@outlook.com>
2022-03-09 00:22:21 +0100

    better prints common modulus

--- tests/test_common_Modulus.py
+++ tests/test_common_Modulus.py
@@ -8,17 +8,18 @@ class MyTestCase(unittest.TestCase):
         n_bits = 1024
         num_users = 2
         e_list = [3, 7]
-        m = 3424242576285276438598359536456789065464646789954353535335
+        m = 342424257628527643859835953645678906546464678995435353533545454
         user_list = gen_users_sameMod(n_bits, num_users, e_list)
 
         c_1 = encrypt(m, user_list[0].e, user_list[0].N)
         c_2 = encrypt(m, user_list[1].e, user_list[1].N)
 
-        print(decrypt(c_2, user_list[1].d, user_list[1].N))
+        # Perform common modulus attack, given c_1, c_2, e_1, e_2 and N
+        result = common_Modulus_Attack(c_1, c_2, user_list[0].e, user_list[1].e, user_list[0].N)
 
-        print(common_Modulus_Attack(c_1, c_2, user_list[0].e, user_list[1].e, user_list[0].N))
-        
-
+        self.assertEqual(m, result)
+        print("Message found: " + str(result))
+        print("Common Modulus Attack was Successful")
 
 
 
